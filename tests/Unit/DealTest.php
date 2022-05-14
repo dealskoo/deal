374e59806fa55ccb8c78a36e377b0f500221c9f5
jabber <2898117012@qq.com>
2022-05-15 04:05:27 +0800

    init

--- tests/Unit/DealTest.php
+++ tests/Unit/DealTest.php
@@ -8,4 +8,54 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class DealTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_cover()
+    {
+
+    }
+
+    public function test_cover_url()
+    {
+
+    }
+
+    public function test_off()
+    {
+
+    }
+
+    public function test_approved()
+    {
+
+    }
+
+    public function test_avaiabled()
+    {
+
+    }
+
+    public function test_base_deals()
+    {
+
+    }
+
+    public function test_big_discount()
+    {
+
+    }
+
+    public function test_free_shipping()
+    {
+
+    }
+
+    public function test_limited_time()
+    {
+
+    }
+
+    public function test_zone()
+    {
+
+    }
 }
