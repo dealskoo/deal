daa397270a415c33180888188d195f14505239d7
jabber <2898117012@qq.com>
2022-05-15 02:30:11 +0800

    init

---
+++ tests/Unit/DealTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dealskoo\Deal\Tests\Unit;
+
+use Dealskoo\Deal\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DealTest extends TestCase
+{
+    use RefreshDatabase;
+}
